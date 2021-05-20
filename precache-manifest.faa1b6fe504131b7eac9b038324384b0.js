433a7c7bcb93cc9147dfbf64cbd3b5f66ddd4841
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-20 05:56:23 -0700

    Updates

---
+++ precache-manifest.faa1b6fe504131b7eac9b038324384b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca1787c0697c6851b3549e694d1ea38",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "d3a3e22489ffadbf644a",
+    "url": "/memegenerator/static/css/main.732aee96.chunk.css"
+  },
+  {
+    "revision": "200ddd3370c229bede64",
+    "url": "/memegenerator/static/js/2.7bef4711.chunk.js"
+  },
+  {
+    "revision": "d3a3e22489ffadbf644a",
+    "url": "/memegenerator/static/js/main.e213b699.chunk.js"
+  },
+  {
+    "revision": "6c9a26c3decfaf1f33de",
+    "url": "/memegenerator/static/js/runtime-main.43ff85c0.js"
+  }
+]);
